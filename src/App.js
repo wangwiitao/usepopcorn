e1cf85e76f4330c4c5e9d134d5258b2b5a83d822
975569220@qq.com <975569220@qq.com>
2025-11-18 20:22:36 +0800

    useEffect example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,9 +50,20 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = "83d3eb62";
+
 export default function App() {
   const [watched, setWatched] = useState(tempWatchedData);
   const [movies, setMovies] = useState(tempMovieData);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.Search);
+      });
+  }, []);
   return (
     <>
       <NavBar>
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = React.useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = React.useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]} />
+    <App />
+    {/* <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]} />
     <StarRating size={24} color="red" defaultRating={3} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
